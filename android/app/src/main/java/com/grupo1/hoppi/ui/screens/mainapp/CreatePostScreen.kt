d95fe88a9e177ba4c59338c882f288ecece4b7c4
caiodanjo <caio.danjo@gmail.com>
2025-10-29 13:28:22 -0300

    Edição de CreatePostScreen

--- android/app/src/main/java/com/grupo1/hoppi/ui/screens/mainapp/CreatePostScreen.kt
+++ android/app/src/main/java/com/grupo1/hoppi/ui/screens/mainapp/CreatePostScreen.kt
@@ -7,11 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
@@ -23,11 +20,8 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -39,6 +33,12 @@ val Rosa = Color(0xFFA4485F)
 val CinzaClaro = Color(0xFFA6A6A6)
 val Branco = Color.White
 val CinzaMuitoClaro = Color(0xFFE0E0E0)
+val VerdeEstudo = Color(0xFF85D39F)
+val AzulVenda = Color(0xFF9CBDC6)
+val LaranjaInfo = Color(0xFFEDC8B1)
+val TextColorEstudo = Color(0xFF00822B)
+val TextColorVenda = Color(0xFF406B8D)
+val TextColorInfo = Color(0xFFEC8445)
 
 data class TagItem(
     val name: String,
@@ -52,13 +52,11 @@ fun CreatePostScreen(navController: NavController) {
     var selectedTag by remember { mutableStateOf<TagItem?>(null) }
     var showMenuTags by remember { mutableStateOf(false) }
     val focusRequester = remember { FocusRequester() }
-    val menuHeightPx = remember { mutableStateOf(0) }
-    val density = LocalDensity.current
 
     val availableTags = listOf(
-        TagItem("Estudo", Color(0xFFB4E4C5), R.drawable.estudo_icon),
-        TagItem("Venda", Color(0xFFC8D8F5), R.drawable.venda_icon),
-        TagItem("Info", Color(0xFFFFE2CC), R.drawable.info_icon)
+        TagItem("Estudo", VerdeEstudo, R.drawable.estudo_icon),
+        TagItem("Venda", AzulVenda, R.drawable.venda_icon),
+        TagItem("Info", LaranjaInfo, R.drawable.info_icon)
     )
 
     LaunchedEffect(Unit) {
@@ -67,9 +65,7 @@ fun CreatePostScreen(navController: NavController) {
     }
 
     Scaffold(
-        topBar = {
-            CreatePostTopBar(onCloseClick = { navController.popBackStack() })
-        }
+        topBar = { CreatePostTopBar(onCloseClick = { navController.popBackStack() }) }
     ) { paddingValues ->
         Box(
             modifier = Modifier
@@ -80,15 +76,15 @@ fun CreatePostScreen(navController: NavController) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
                     .imePadding()
                     .padding(horizontal = 20.dp)
             ) {
                 Row(
                     modifier = Modifier
                         .padding(top = 20.dp)
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
+                        .fillMaxWidth()
+                        .weight(1f),
+                    verticalAlignment = Alignment.Top
                 ) {
                     Box(
                         modifier = Modifier
@@ -96,93 +92,98 @@ fun CreatePostScreen(navController: NavController) {
                             .background(Rosa, RoundedCornerShape(25.dp))
                     )
                     Spacer(modifier = Modifier.width(10.dp))
+
                     BasicTextField(
                         value = postText,
                         onValueChange = { if (it.length <= 1000) postText = it },
                         modifier = Modifier
-                            .weight(1f)
+                            .fillMaxSize()
                             .focusRequester(focusRequester),
                         textStyle = TextStyle(fontSize = 16.sp, color = Color.Black),
                         decorationBox = { innerTextField ->
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(vertical = 12.dp)
+                            Column(
+                                modifier = Modifier.fillMaxSize()
                             ) {
-                                if (postText.isEmpty()) {
-                                    Text(
-                                        text = "O que você está pensando?",
-                                        color = CinzaClaro,
-                                        fontSize = 16.sp
-                                    )
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .weight(1f)
+                                        .padding(vertical = 12.dp)
+                                ) {
+                                    if (postText.isEmpty()) {
+                                        Text(
+                                            text = "O que você está pensando?",
+                                            color = CinzaClaro,
+                                            fontSize = 16.sp,
+                                        )
+                                    }
+                                    innerTextField()
                                 }
-                                innerTextField()
                             }
                         }
                     )
                 }
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(250.dp))
                 Divider(color = Color.Black, thickness = 1.dp)
                 Spacer(modifier = Modifier.weight(1f))
-            }
 
-            Box(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .background(Color.White)
-                    .imePadding()
-                    .padding(horizontal = 20.dp, vertical = 8.dp)
-            ) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    AnimatedVisibility(
-                        visible = showMenuTags,
-                        enter = expandVertically(expandFrom = Alignment.Bottom),
-                        exit = shrinkVertically(shrinkTowards = Alignment.Bottom),
-                        modifier = Modifier
-                            .align(Alignment.Start)
-                            .offset {
-                                val offsetDp = with(density) { (menuHeightPx.value.toDp() + 8.dp).roundToPx() }
-                                IntOffset(0, -offsetDp)
-                            }
-                    ) {
-                        Column(
+                Box(
+                    modifier = Modifier
+                        .background(Color.White)
+                        .padding(vertical = 8.dp)
+                        .fillMaxWidth()
+                ) {
+                    Column {
+                        AnimatedVisibility(
+                            visible = showMenuTags,
                             modifier = Modifier
-                                .width(150.dp)
-                                .background(Color.White, RoundedCornerShape(10.dp))
-                                .shadow(4.dp, RoundedCornerShape(10.dp))
-                                .onSizeChanged { size -> menuHeightPx.value = size.height }
-                                .padding(vertical = 8.dp)
+                                .offset(x = 5.dp, y = (-10).dp),
+                            enter = expandVertically(expandFrom = Alignment.Bottom),
+                            exit = shrinkVertically(shrinkTowards = Alignment.Bottom)
                         ) {
-                            availableTags.forEach { tag ->
-                                TagMenuItem(
-                                    tag = tag,
-                                    onClick = {
-                                        selectedTag = it
-                                        showMenuTags = false
+                            Column(
+                                modifier = Modifier
+                                    .width(IntrinsicSize.Max)
+                                    .shadow(8.dp, RoundedCornerShape(10.dp))
+                                    .background(Color.White, RoundedCornerShape(10.dp))
+                                    .padding(12.dp, 16.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ){
+                                availableTags.forEach { tag ->
+                                    TagMenuItem(
+                                        tag = tag,
+                                        onClick = {
+                                            selectedTag = it
+                                            showMenuTags = false
+                                        }
+                                    )
+                                    if (tag != availableTags.last()) {
+                                        Spacer(modifier = Modifier.height(10.dp))
                                     }
+                                }
+                            }
+                        }
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                selectedTag?.let { tag ->
+                                    TagDisplay(tag = tag, onDismiss = { selectedTag = null })
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                }
+                                AdicionarTagButton(
+                                    onClick = { showMenuTags = !showMenuTags }
                                 )
                             }
+                            Text(
+                                text = "${postText.length}/1000 caracteres",
+                                color = CinzaClaro,
+                                fontSize = 12.sp
+                            )
                         }
-                    }
-
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            selectedTag?.let { tag ->
-                                TagDisplay(tag = tag, onDismiss = { selectedTag = null })
-                                Spacer(modifier = Modifier.width(8.dp))
-                            }
-                            AdicionarTagButton(onClick = { showMenuTags = !showMenuTags })
-                        }
-
-                        Text(
-                            text = "${postText.length}/1000 caracteres",
-                            color = CinzaClaro,
-                            fontSize = 12.sp
-                        )
                     }
                 }
             }
@@ -214,6 +215,13 @@ fun CreatePostTopBar(onCloseClick: () -> Unit) {
 
 @Composable
 fun TagDisplay(tag: TagItem, onDismiss: () -> Unit) {
+    val textColor = when (tag.name) {
+        "Estudo" -> TextColorEstudo
+        "Venda" -> TextColorVenda
+        "Info" -> TextColorInfo
+        else -> Color.White
+    }
+
     Row(
         modifier = Modifier
             .background(tag.color, RoundedCornerShape(4.dp))
@@ -222,12 +230,12 @@ fun TagDisplay(tag: TagItem, onDismiss: () -> Unit) {
     ) {
         Image(painter = painterResource(id = tag.iconRes), contentDescription = null, modifier = Modifier.size(16.dp))
         Spacer(modifier = Modifier.width(4.dp))
-        Text(tag.name, color = Color.Black, fontSize = 14.sp)
+        Text(tag.name, color = textColor, fontSize = 14.sp)
         Spacer(modifier = Modifier.width(4.dp))
         Icon(
             Icons.Default.Close,
             contentDescription = "Remover Tag",
-            tint = Color.Black,
+            tint = textColor,
             modifier = Modifier
                 .size(16.dp)
                 .clickable(onClick = onDismiss)
@@ -237,28 +245,36 @@ fun TagDisplay(tag: TagItem, onDismiss: () -> Unit) {
 
 @Composable
 fun TagMenuItem(tag: TagItem, onClick: (TagItem) -> Unit) {
+    val textColor = when (tag.name) {
+        "Estudo" -> TextColorEstudo
+        "Venda" -> TextColorVenda
+        "Info" -> TextColorInfo
+        else -> Color.White
+    }
+
     Row(
         modifier = Modifier
-            .fillMaxWidth()
+            .width(95.dp)
             .clickable { onClick(tag) }
-            .background(tag.color.copy(alpha = 0.4f), RoundedCornerShape(6.dp))
-            .padding(horizontal = 12.dp, vertical = 10.dp),
+            .background(tag.color.copy(alpha = 0.9f), RoundedCornerShape(5.dp))
+            .padding(horizontal = 8.dp, vertical = 5.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(painter = painterResource(id = tag.iconRes), contentDescription = null, modifier = Modifier.size(20.dp))
         Spacer(modifier = Modifier.width(10.dp))
-        Text(tag.name, color = Color.Black, fontSize = 15.sp)
+        Text(tag.name, color = textColor, fontSize = 14.sp)
     }
 }
 
 @Composable
 fun AdicionarTagButton(onClick: () -> Unit) {
     val buttonBackgroundColor = CinzaMuitoClaro.copy(alpha = 0.6f)
+
     Row(
         modifier = Modifier
             .background(buttonBackgroundColor, RoundedCornerShape(5.dp))
             .clickable(onClick = onClick)
-            .padding(horizontal = 6.dp, vertical = 4.dp),
+            .padding(horizontal = 8.dp, vertical = 6.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(Icons.Default.Add, contentDescription = "Adicionar Tag", tint = CinzaClaro, modifier = Modifier.size(18.dp))
