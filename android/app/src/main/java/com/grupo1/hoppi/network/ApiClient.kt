3d12dddfa3b19a635d03d063c3f25b9085e41b61
caiodanjo <caio.danjo@gmail.com>
2025-12-04 18:20:21 -0300

    Edição no ApiClient

--- android/app/src/main/java/com/grupo1/hoppi/network/ApiClient.kt
+++ android/app/src/main/java/com/grupo1/hoppi/network/ApiClient.kt
@@ -13,8 +13,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2:3000/"
-    //private const val BASE_URL = "https://hoppi.onrender.com/"
+    //private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "https://hoppi.onrender.com/"
 
     private val json = Json {
         ignoreUnknownKeys = true
